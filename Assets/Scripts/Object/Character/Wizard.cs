5813e94cbbfc71d2cb2a1b9901288b05bc7d18b9
Geun Hee Park <penspanic@gmail.com>
2016-12-04 03:37:26 +0900

    EffectController 추가

--- Assets/Scripts/Object/Character/Wizard.cs
+++ Assets/Scripts/Object/Character/Wizard.cs
@@ -12,7 +12,8 @@ public class Wizard : Character
     protected override void DoAction()
     {
         base.DoAction();
-        SoundManager.instance.PlayEffectSound(SoundManager.Effect.Wizard_Hit);   
+        SoundManager.instance.PlayEffectSound(SoundManager.Effect.Wizard_Hit);
+        effectCtrler.ShowEffect(EffectType.WizardSkill, 1f, transform.position);
     }
 
     public override void SetBuff(float duration, float ratio)
