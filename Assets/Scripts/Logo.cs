56a14a985c5f1963210f4ffada1d19d1b885c421
kdsaxx <vvx35@naver.com>
2016-12-03 20:28:25 +0900

    사운드매니져 로고

---
+++ Assets/Scripts/Logo.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Logo : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Awake()
+    {
+        SoundManager.instance.LoadSound();
+    }
+    void Start()
+    {
+        SoundManager.instance.PlayBgmSound(SoundManager.BGM.Logo);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(SceneController.instance.FadeOut(1, "Menu"));
+        }
+
+
+
+    }
+}
